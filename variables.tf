71926fbeaecf1ada82d36d3553666ef5f3ac45e6
Abdulk777 <113355669+Abdulk777@users.noreply.github.com>
2024-04-29 15:52:58 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "appservice_windows" {
+    type = map(object({
+      name = string
+      resource_group_name = string
+      location = string
+      service_plan_id = string
+    }))
+    default = {
+    }
+}
